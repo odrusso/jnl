3c5241f2e25a3e55937ed1061e7a822e53ee11e9
Oscar Russo <odrusso1@gmail.com>
2021-02-08 22:31:53 +1300

    WIP crypto

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,9 +1,36 @@
-# import requests
+import bcrypt
+import json
+
+PIGEON_HOLE_PASSWORD = "pigeonHolePass"
+
+
 def main(event, context):
-    # response = requests.get("https://docs.aws.amazon.com")
-    # print(response.text)
+    body = json.loads(event)
+
+    input_password = body[PIGEON_HOLE_PASSWORD]
+
+    password_status = verify_password_for_pigeonhole("TODO", input_password)
+
+    print(f"Password Check {password_status}")
     return "Working!"
 
 
+def verify_password_for_pigeonhole(pigeon_hole_name: str, pigeon_hole_password: str) -> bool:
+    input_bytes = str.encode(pigeon_hole_password)
+    hash_to_check_against = get_hash_for_pigeonhole(pigeon_hole_name)
+    return bcrypt.checkpw(input_bytes, hash_to_check_against)
+
+
+def get_hash_for_pigeonhole(pigeon_hole_name: str) -> bytes:
+    # TODO: Fetch real hashes!
+    pretend_salt = bcrypt.gensalt()
+    return bcrypt.hashpw(b'password', pretend_salt)
+
+
 if __name__ == "__main__":
-    main('', '')
+    body = json.dumps(
+        {
+            "pigeonHolePass": "password"
+        }
+    )
+    main(body, '')
---
+++ app/run.sh
@@ -0,0 +1,5 @@
+mv lambda_function.py package
+cd package
+python3 lambda_function.py
+mv lambda_function.py ../
+cd ../
