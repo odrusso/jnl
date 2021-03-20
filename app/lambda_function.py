c265766d040a492445293e38d48c9a2ad6fe78d8
Oscar Russo <odrusso1@gmail.com>
2021-03-20 22:40:35 +1300

    Change request parsing to match API Gateway

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -21,34 +21,43 @@ class HttpResponse:
 
 
 def main(event, context):
+    body = json.loads(event['body'])
+    method = event['httpMethod']
+    if method == 'POST':
+        return get_pigeonhole(body).json()
+
+
+def get_pigeonhole(body) -> HttpResponse:
     try:
-        input_password = event[PIGEON_HOLE_PASSWORD_KEY]
-        input_pigeon_hole_name = event[PIGEON_HOLE_NAME_KEY]
+        input_password = body[PIGEON_HOLE_PASSWORD_KEY]
+        input_pigeon_hole_name = body[PIGEON_HOLE_NAME_KEY]
         # input_messages = event[MESSAGES_KEY] # for POST only
     except KeyError:
         # Probably the body doesn't conform to the expected schema
-        return HttpResponse("Invalid request body", 400).json()
+        return HttpResponse("Invalid request body", 400)
 
     pigeonhole = get_pigeonhole_data(input_pigeon_hole_name)
 
     if pigeonhole is None:
         print(f"Invalid pigeon hole name: {input_pigeon_hole_name}")
-        return HttpResponse("Invalid request body", 403).json()
+        return HttpResponse("Invalid request body", 403)
 
     password_valid = verify_password_for_pigeonhole(input_pigeon_hole_name, input_password)
 
     if password_valid:
-        return HttpResponse(pigeonhole['data'], 200).json()
+        return HttpResponse(pigeonhole['data'], 200)
     else:
         # invalid password
         print(f"Invalid password for pigeonhole {input_pigeon_hole_name}")
-        return HttpResponse("Invalid request body", 403).json()
+        return HttpResponse("Invalid request body", 403)
 
 
 if __name__ == "__main__":
     test_body = {
-        "pigeonHolePass": "password",
-        "pigeonHoleName": "test"
+        "body": {
+            "pigeonHolePass": "password",
+            "pigeonHoleName": "test"
+        }
     }
 
     print(main(test_body, ''))
