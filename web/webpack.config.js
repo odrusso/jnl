d6a7f6c9e9851509f6cd88a663381de73a6c84ce
Oscar Russo <odrusso1@gmail.com>
2021-03-21 17:21:52 +1300

    Fix build issue

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    watch: true,
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
