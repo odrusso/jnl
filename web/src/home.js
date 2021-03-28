e6cd51affd17d3e61ba80b58e1a938649fb00e74
Oscar Russo <odrusso1@gmail.com>
2021-03-28 21:13:53 +1300

    Add contribution link

--- web/src/home.js
+++ web/src/home.js
@@ -5,6 +5,11 @@ export function Home(props) {
     return (
         <>
             <App/>
+            <a
+                href="https://github.com/odrusso/jnl"
+                target="__blank"
+                className={"contribute d-none d-xl-block p-2"}
+            >contribute</a>
         </>
     )
 }
