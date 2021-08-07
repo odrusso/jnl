1f53737243b2c309698faf2242e559052fda20b6
Oscar Russo <odrusso1@gmail.com>
2021-08-07 17:04:50 +1200

    Refactor JNLEntry outside of main app, and add some tests

---
+++ web/src/components/JNLEntry.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import {CheckCircleFill} from "react-bootstrap-icons";
+
+export const JNLEntry = ({addMessage}) => {
+
+    const [message, setMessage] = useState('')
+
+    const handleSubmit = (e) => {
+        // Appends message to messages
+        if (message !== '') {
+            let now = new Date()
+            let timeString = `${now.getHours()}:${now.getMinutes()} ${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
+            console.log(timeString)
+            addMessage(message, timeString)
+        }
+        setMessage('')
+        e.preventDefault()
+    }
+
+    return (
+        <div className={'form-area'}>
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    className="form-text p-4"
+                    key="jnl-text-area"
+                    value={message}
+                    data-testid={"entry-message"}
+                    onChange={(e) => setMessage(e.target.value)}/>
+                <br/>
+                <br/>
+
+                <div style={{textAlign: "center"}}>
+                    <h2>
+                        <CheckCircleFill data-testid={"submit-message"} onClick={handleSubmit}/>
+                    </h2>
+                </div>
+            </form>
+        </div>
+    )
+}
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,12 +1,16 @@
+// eslint-disable-next-line no-undef
 const path = require('path');
+// eslint-disable-next-line no-undef
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+// eslint-disable-next-line no-undef
 module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
     resolve: {
+        extensions: [".jsx", ".js"],
         modules: [path.join(__dirname, 'src'), 'node_modules'],
         alias: {
             react: path.join(__dirname, 'node_modules', 'react'),
