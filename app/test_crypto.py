e5ecd483d8b4e7a1372fd592775e60e1463c7782
Oscar Russo <odrusso1@gmail.com>
2021-08-08 12:54:47 +1200

    Fix dir structure

--- app/test_crypto.py
+++ app/test_crypto.py
@@ -1,18 +1,18 @@
 import unittest
-import app.crypto
+
+from crypto import create_new_hash_for_password
 
 
 class CryptoUnitTests(unittest.TestCase):
     def test_can_create_hash_for_password_string(self):
-        saltb64, hashb64 = app.crypto.create_new_hash_for_password("password")
+        saltb64, hashb64 = create_new_hash_for_password("password")
         self.assertEqual(type(saltb64), type(b''))
         self.assertEqual(type(hashb64), type(b''))
         self.assertNotEqual(saltb64, b'')
         self.assertNotEqual(hashb64, b'')
 
     def test_creates_different_hases_on_subsequent_runs(self):
-        saltb64, hashb64 = app.crypto.create_new_hash_for_password("password")
-        saltb64_1, hashb64_1 = app.crypto.create_new_hash_for_password("password")
+        saltb64, hashb64 = create_new_hash_for_password("password")
+        saltb64_1, hashb64_1 = create_new_hash_for_password("password")
         self.assertNotEqual(saltb64, saltb64_1)
         self.assertNotEqual(hashb64, hashb64_1)
-
