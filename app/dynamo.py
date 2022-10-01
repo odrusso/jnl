6dcbad6ec60575e8a1987224d69adc57613a4d6d
Oscar <odrusso1@gmail.com>
2022-10-01 19:12:53 +1300

    Tidy up pre master merge

--- app/dynamo.py
+++ app/dynamo.py
@@ -28,7 +28,6 @@ def get_pigeonhole_data(pigeonhole_name: str) -> Optional[dict]:
 
 def new_pigeonhole(pigeonhole_name: str, hash: str, salt: str, data: str):
     db = boto3.resource("dynamodb")
-    tableName = os.environ.get('dynamoTableName', default=False) or "jnl-data"
     table = db.Table(get_tableName())
 
     table.put_item(
